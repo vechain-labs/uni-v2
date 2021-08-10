424fa9e2a2206df1d3da76a5a77318838592bc2f
laalaguer <laalaguer@gmail.com>
2021-08-10 14:26:15 +0800

    Remove loyalty program from the main branch

--- uniswap-v2-core/contracts/interfaces/ILoyalty.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity =0.5.16;
-
-interface ILoyalty {
-    function addPoints (address _who, uint256 _amount) external;
-    function removePoints (address _who, uint256 _amount) external;
-}
